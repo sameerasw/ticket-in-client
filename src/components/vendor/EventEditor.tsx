2a5ea43a343f9048598be6e92f36300d5cc17caa
sameerasw <sameera.whoami@gmail.com>
2024-12-06 01:02:36 +0530

    Implemented web socket for real time ticket purchase lgos and available tickets updates for the events

--- src/components/vendor/EventEditor.tsx
+++ src/components/vendor/EventEditor.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -9,14 +9,14 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTheme } from '@mui/material/styles';
 import { Event } from '../../types/Event';
 import { TransitionProps } from '@mui/material/transitions';
-import { alpha, Grow, TextField, Box, Typography, CircularProgress } from '@mui/material';
+import { alpha, Grow, TextField, Box, Typography, CircularProgress, Card } from '@mui/material';
 import { releaseTickets } from '../../services/vendorApi';
+import { useWebSocket } from '../../hooks/useWebSocket';
 
 interface EventEditorProps {
     open: boolean;
     onClose: () => void;
     event: Event | null;
-    onSave: (event: Event) => void;
     vendorId: number | null;
     vendorName: string | null;
     fetchVendorEvents: () => void;
@@ -31,104 +31,14 @@ const Transition = React.forwardRef(function Transition(
     return <Grow ref={ref} {...props} />;
 });
 
-const EventEditor: React.FC<EventEditorProps> = ({ open, onClose, event, onSave, vendorId, vendorName, fetchVendorEvents }) => {
+const EventEditor: React.FC<EventEditorProps> = ({ open, onClose, event, vendorId, vendorName, fetchVendorEvents }) => {
     const theme = useTheme();
     const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
 
-    const [eventName, setEventName] = useState('');
-    const [eventLocation, setEventLocation] = useState('');
-    const [eventDate, setEventDate] = useState('');
-    const [eventTime, setEventTime] = useState('');
-    const [ticketPrice, setTicketPrice] = useState('');
-    const [details, setDetails] = useState('');
-    const [image, setImage] = useState('');
     const [ticketCount, setTicketCount] = useState<number>(1);
     const [loading, setLoading] = useState<boolean>(false);
 
-    const [errors, setErrors] = useState({
-        eventName: '',
-        eventLocation: '',
-        eventDate: '',
-        eventTime: '',
-        ticketPrice: '',
-        details: '',
-        image: '',
-    });
-
-    useEffect(() => {
-        if (event) {
-            setEventName(event.eventName);
-            setEventLocation(event.eventLocation);
-            setEventDate(event.eventDate);
-            setEventTime(event.eventTime);
-            setTicketPrice(event.ticketPrice.toString());
-            setDetails(event.details);
-            setImage(event.image);
-        } else {
-            setEventName('');
-            setEventLocation('');
-            setEventDate('');
-            setEventTime('');
-            setTicketPrice('');
-            setDetails('');
-            setImage('');
-        }
-    }, [event, open]);
-
-    const validate = () => {
-        let tempErrors = { ...errors };
-        tempErrors.eventName = eventName.length > 500 ? 'Event Name cannot exceed 500 characters' : '';
-        tempErrors.eventLocation = eventLocation.length > 500 ? 'Event Location cannot exceed 500 characters' : '';
-        tempErrors.eventDate = eventDate ? '' : 'Event Date is required';
-        tempErrors.eventTime = eventTime ? '' : 'Event Time is required';
-        tempErrors.ticketPrice = ticketPrice && parseFloat(ticketPrice) > 0 ? '' : 'Ticket Price must be a positive number';
-        tempErrors.details = details.length > 2000 ? 'Details cannot exceed 2000 characters' : '';
-        tempErrors.image = image.length > 1000 ? 'Image URL cannot exceed 1000 characters' : '';
-        setErrors(tempErrors);
-        return Object.values(tempErrors).every(x => x === '');
-    };
-
-    const handleSave = async () => {
-        if (validate()) {
-            setLoading(true);
-            try {
-                if (event) {
-                    const updatedEvent: Event = {
-                        ...event,
-                        eventName,
-                        eventLocation,
-                        eventDate,
-                        eventTime,
-                        ticketPrice: parseFloat(ticketPrice),
-                        details,
-                        image,
-                    };
-                    await onSave(updatedEvent);
-                } else {
-                    if (vendorId && vendorName) {
-                        const newEvent: Event = {
-                            eventName,
-                            eventLocation,
-                            eventDate,
-                            eventTime,
-                            ticketPrice: parseFloat(ticketPrice),
-                            details,
-                            image,
-                            vendorId: vendorId,
-                            vendorName: vendorName,
-                        };
-                        await onSave(newEvent);
-                    } else {
-                        console.error('Vendor ID or Name not found');
-                    }
-                }
-            } catch (error) {
-                console.error('Error saving event:', error);
-            } finally {
-                setLoading(false);
-            }
-        }
-    };
+    const { purchases, availableTickets, socket } = useWebSocket(event?.id || null);
 
     const handleReleaseTickets = async () => {
         if (event && ticketCount > 0 && event.id) {
@@ -146,13 +56,17 @@ const EventEditor: React.FC<EventEditorProps> = ({ open, onClose, event, onSave,
         }
     };
 
+    const handleDialogClose = () => {
+        onClose();
+    };
+
     return (
         <Dialog
             fullScreen={fullScreen}
             open={open}
             TransitionComponent={Transition}
             keepMounted
-            onClose={onClose}
+            onClose={handleDialogClose}
             aria-labelledby="responsive-dialog-title"
             sx={{
                 backdropFilter: "blur(20px)",
@@ -162,140 +76,65 @@ const EventEditor: React.FC<EventEditorProps> = ({ open, onClose, event, onSave,
             }}
         >
             <DialogTitle id="responsive-dialog-title">
-                {event ? 'Edit Event' : 'Create Event'}
+                {event ? `Manage Event: ${event.eventName}` : 'Manage Event'}
             </DialogTitle>
             <DialogContent>
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="eventName"
-                    label="Event Name"
-                    name="eventName"
-                    autoComplete="eventName"
-                    autoFocus
-                    value={eventName}
-                    onChange={(e) => setEventName(e.target.value)}
-                    error={!!errors.eventName}
-                    helperText={errors.eventName}
-                />
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="eventLocation"
-                    label="Event Location"
-                    name="eventLocation"
-                    autoComplete="eventLocation"
-                    value={eventLocation}
-                    onChange={(e) => setEventLocation(e.target.value)}
-                    error={!!errors.eventLocation}
-                    helperText={errors.eventLocation}
-                />
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="eventDate"
-                    label="Event Date"
-                    name="eventDate"
-                    type="date"
-                    InputLabelProps={{ shrink: true }}
-                    value={eventDate}
-                    onChange={(e) => setEventDate(e.target.value)}
-                    error={!!errors.eventDate}
-                    helperText={errors.eventDate}
-                />
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="eventTime"
-                    label="Event Time"
-                    name="eventTime"
-                    type="time"
-                    InputLabelProps={{ shrink: true }}
-                    value={eventTime}
-                    onChange={(e) => setEventTime(e.target.value)}
-                    error={!!errors.eventTime}
-                    helperText={errors.eventTime}
-                />
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="ticketPrice"
-                    label="Ticket Price"
-                    name="ticketPrice"
-                    type="number"
-                    value={ticketPrice}
-                    onChange={(e) => setTicketPrice(e.target.value)}
-                    error={!!errors.ticketPrice}
-                    helperText={errors.ticketPrice}
-                />
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="details"
-                    label="Details"
-                    name="details"
-                    multiline
-                    rows={4}
-                    value={details}
-                    onChange={(e) => setDetails(e.target.value)}
-                    error={!!errors.details}
-                    helperText={errors.details}
-                />
-                <TextField
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="image"
-                    label="Image URL"
-                    name="image"
-                    value={image}
-                    onChange={(e) => setImage(e.target.value)}
-                    error={!!errors.image}
-                    helperText={errors.image}
-                />
                 {event && (
-                    <Box sx={{ marginTop: 2 }}>
-                        <Typography variant="h6">Release New Tickets</Typography>
-                        <Box sx={{
-                            display: 'flex',
-                            flexDirection: 'rows',
-                            gap: '0.5rem',
-                            alignItems: 'center',
-                        }}>
-                            <TextField
-                                margin="normal"
-                                fullWidth
-                                id="ticketCount"
-                                label="Number of Tickets"
-                                name="ticketCount"
-                                type="number"
-                                value={ticketCount}
-                                onChange={(e) => setTicketCount(parseInt(e.target.value))}
-                            />
-                            <Typography variant="body2" color="textSecondary" sx={{
-                                marginLeft: '1rem',
+                    <>
+                        <Box sx={{ marginTop: 2 }}>
+                            <Typography variant="h6">Release New Tickets</Typography>
+                            <Box sx={{
+                                display: 'flex',
+                                flexDirection: 'rows',
+                                gap: '0.5rem',
+                                alignItems: 'center',
                             }}>
-                                Available: {event.availableTickets}
-                            </Typography>
+                                <TextField
+                                    margin="normal"
+                                    fullWidth
+                                    id="ticketCount"
+                                    label="Number of Tickets"
+                                    name="ticketCount"
+                                    type="number"
+                                    value={ticketCount}
+                                    onChange={(e) => setTicketCount(parseInt(e.target.value))}
+                                />
+                                <Typography variant="body2" color="textSecondary" sx={{
+                                    marginLeft: '1rem',
+                                }}>
+                                    Available: {availableTickets}
+                                </Typography>
+                            </Box>
+                            <Button variant="contained" onClick={handleReleaseTickets} disabled={loading}>
+                                {loading ? <CircularProgress size={24} /> : 'Release Tickets'}
+                            </Button>
                         </Box>
-                        <Button variant="contained" onClick={handleReleaseTickets} disabled={loading}>
-                            {loading ? <CircularProgress size={24} /> : 'Release Tickets'}
-                        </Button>
-                    </Box>
+                        <Box sx={{ marginTop: 4 }}>
+                            <Typography variant="h6" sx={{ textAlign: 'center', marginY: '2rem' }}>
+                                Real-Time Ticket Purchases Log
+                            </Typography>
+                            <Box sx={{ overflowY: 'scroll', height: '60vh', padding: '1rem' }}>
+                                {purchases.map((purchase, index) => (
+                                    <Card key={index} sx={{ marginBottom: '1rem', padding: '1rem' }}>
+                                        🎫 {purchase}
+                                    </Card>
+                                ))}
+                                <Card sx={{
+                                    marginBottom: '1rem',
+                                    padding: '1rem',
+                                    flexGrow: 1,
+                                    backgroundColor: 'background.default',
+                                }} variant='outlined'>
+                                    Connection status: {socket?.readyState === WebSocket.OPEN ? '🟢' : '🔴'} Ticket purchasing information will be displayed here in real-time.
+                                </Card>
+                            </Box>
+                        </Box>
+                    </>
                 )}
             </DialogContent>
             <DialogActions>
-                <Button onClick={onClose} disabled={loading}>
-                    Cancel
-                </Button>
-                <Button onClick={handleSave} variant="contained" disabled={loading}>
-                    {loading ? <CircularProgress size={24} /> : 'Save'}
+                <Button onClick={handleDialogClose} disabled={loading}>
+                    Close
                 </Button>
             </DialogActions>
         </Dialog>
